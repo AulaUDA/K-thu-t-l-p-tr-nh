ea907279d0d0fc8b5df6571b46bb1153640c2c09
Phạm Chí Thiên <thien107582@donga.edu.vn>
2024-10-29 21:52:24 +0700

    Add files via upload

---
+++ 4. Tuần 4/Thien107582_4_2CB.java
@@ -0,0 +1,16 @@
+package phamchithien.buoi4;
+
+public class Thien107582_4_2CB {
+    public static void bai4_2(){
+        String nguHanh[] = {"kim","moc","thuy","hoa","tho"};
+        
+        for (int css = 0; css < nguHanh.length; css++ )
+        {
+            System.out.printf("Phan tu thu nguHanh[%d] = %s\n",css +1, css , nguHanh[css]);
+        }
+
+    }
+    public static void main(String[] args) {
+        bai4_2();
+    }
+}
