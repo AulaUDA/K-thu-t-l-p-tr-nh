fdd57cef977d6e594d112f7ebef0ba0382ee6cbe
Phạm Chí Thiên <thien107582@donga.edu.vn>
2024-10-29 20:52:08 +0700

    Add files via upload

---
+++ 4. Tuần 4/Thien107582_4_1CB.java
@@ -0,0 +1,15 @@
+package phamchithien.buoi4;
+
+public class Thien107582_4_1CB {
+    public static void bai4_1CB() {
+        int arr[] = {3,5,8,10,14,25};
+
+        for (int cs = 0; cs < arr.length; cs ++)
+        {
+            System.out.printf("Phan tu thu %d arr[%d] = %d\n", cs + 1 , cs ,arr[cs]);
+        }
+    }
+    public static void main(String[] args) {
+        bai4_1CB();
+    }
+}
