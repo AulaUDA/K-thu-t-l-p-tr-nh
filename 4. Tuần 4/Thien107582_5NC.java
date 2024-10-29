1264ace125cc9d61ce31f2115232b278ae9cd11f
Phạm Chí Thiên <thien107582@donga.edu.vn>
2024-10-29 23:41:04 +0700

    Add files via upload

---
+++ 4. Tuần 4/Thien107582_5NC.java
@@ -0,0 +1,22 @@
+package phamchithien.buoi4;
+
+import java.util.Scanner;
+
+public class Thien107582_5NC {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Nhap mot so nguyen: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i < n; i++)
+            if (i % 2 !=0)
+                
+        {
+            System.out.println(i + " ");
+        }
+        scanner.close();
+    }
+    
+}
